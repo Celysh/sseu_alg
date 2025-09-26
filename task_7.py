defef14b387fd0fc9d5a49db2816864f38876b3b
Celysh <korgig71@gmail.com>
2025-09-26 12:54:11 +0400

    Update task_7.py

--- task_7.py
+++ task_7.py
@@ -1,13 +1,19 @@
 n = int(input())
 list = []
+max1 = float('-inf')
+min1 = float('inf')
+
 for i in range(n):
-    nums = int(input(":"))
+    nums = int(input(':'))
+    if nums > max1:
+        max1 = nums
+    if nums < min1:
+        min1 = nums
     list.append(nums)
 
-list.sort()
-i = 1
+h = list.index(min1)
 sum = 0
-for i in range(len(list) - 1):
-    sum += list[i]
-
+while h < list.index(max1):
+    sum += list[h]
+    h += 1
 print(sum)
